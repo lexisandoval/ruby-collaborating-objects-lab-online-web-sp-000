0e88b7ab0b30c54e8e7319d589738558d5e5bf0f
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-01 23:04:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,19 +17,18 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
-    song = self.new
-    song.name = filename.split(" - ")[1]
-    song.artist_name = filename[0]
-    song
+  def self.new_by_filename(file)
+     artist_name = file.split(" - ")[0]
+     song_name = file.split(" - ")[1]
+     song = Song.new(song_name)
+     song.artist_name = artist_name
+     song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+     artist = Artist.find_or_create_by_name(name)
+     self.artist = artist
+     artist.add_song(self)
   end
 
 end
